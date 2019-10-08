848fbaf8ef9499a267f3e074c486ef6126861ed8
Joellehelm <joellenhelm@gmail.com>
2019-10-08 15:27:23 -0500

    got it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -7,18 +8,25 @@ class App < Sinatra::Base
     get '/' do
         erb :super_hero
     end
+    
+  
 
-    post '/team' do
+    post '/teams' do
         @team_name = params[:team][:name]
         @team_motto = params[:team][:motto]
-        @hero_name = team[][]
-        @hero_power =
-        @hero_biography =
-        @team_member =
+        # @hero_name = params[:team][:members][][:name]
+        # @hero_power = []
+        # @hero_biography = []
+        @team_member = params[:team][:members]
 
-
-
+     
         erb :team
     end
+
+    # get '/teams' do
+    #     erb :team
+    # end
+
+   
 
 end
